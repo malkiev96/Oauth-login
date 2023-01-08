a16592cc8740dcf48a515169d18bd235f3ff4005
Leonid <malkiev96@gmail.com>
2023-01-08 16:00:06 +0500

    Add json mixin

--- client-app/src/main/java/ru/malkiev/oauth/controller/AuthorizationController.java
+++ client-app/src/main/java/ru/malkiev/oauth/controller/AuthorizationController.java
@@ -1,6 +1,5 @@
 package ru.malkiev.oauth.controller;
 
-import java.security.Principal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthorizationController {
 
   @GetMapping("/")
-  public String hello(Principal principal) {
-    return "Hello, " + principal.getName();
+  public String hello() {
+    return "Hello";
   }
 }
